8419e65fe1f334edc920ba9e8477aae75284f97c
RxhN <415458401@qq.com>
2016-09-26 09:31:24 +0800

    Update test.js

--- test.js
+++ test.js
@@ -17,7 +17,7 @@
     var descriptor = {
         blocks: [
             // Block type, block name, function name
-            [' ', 'my first block', 'my_first_block'],
+            [' ', 'my  block', 'my_first_block'],
         ]
     };
 
